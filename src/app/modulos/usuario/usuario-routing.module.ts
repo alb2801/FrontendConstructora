59a5396d1636046e2db5785f947e6e23f208e64f
Jhonatan David Garcia <jhonatan.1701811787@ucaldas.edu.co>
2021-07-05 18:21:34 -0500

    ajustes en el formulario y enrutamiento de usuario

--- src/app/modulos/usuario/usuario-routing.module.ts
+++ src/app/modulos/usuario/usuario-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
+import { EliminarUsuarioComponent } from './eliminar-usuario/eliminar-usuario.component';
+import { ListarUsuarioComponent } from './listar-usuario/listar-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crear-usuario',
+    component: CrearUsuarioComponent
+  },
+  {
+    path: 'editar.usuario',
+    component: EditarUsuarioComponent
+  },
+  {
+    path: 'eliminar-ususario',
+    component: EliminarUsuarioComponent
+  },
+  {
+    path: 'listar-usuario',
+    component: ListarUsuarioComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
