c18fa04d2bab3cf0c8129000109eaa0a140d14e0
Jhonatan David Garcia <jhonatan.1701811787@ucaldas.edu.co>
2021-07-14 00:05:30 -0500

    arreglando daños de posada 2.0

--- src/app/modulos/seguridad/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/modulos/seguridad/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-//import * as crypto from 'crypto-js';
+import * as crypto from 'crypto-js';
 import { UsuarioModelo } from 'src/app/modelos/usuario.modelo';
 import { SeguridadService } from 'src/app/servicios/seguridad.service'
 
@@ -41,10 +41,10 @@ export class IniciarSesionComponent implements OnInit {
     } else {
       let usuario = this.ObtenerFgvalidador.usuario.value;
       let clave = this.ObtenerFgvalidador.clave.value;
-      //let claveCifrada = crypto.MD5(clave).toString();
+      let claveCifrada = crypto.MD5(clave).toString();
       let modelo = new UsuarioModelo();
       modelo.Correo = usuario;
-      //modelo.Contrasena = claveCifrada;
+      modelo.Contrasena = claveCifrada;
       this.servicioSeguridad.VerificarUsuario(modelo).subscribe(
         (datos) => {
           alert("Datos correctos");
