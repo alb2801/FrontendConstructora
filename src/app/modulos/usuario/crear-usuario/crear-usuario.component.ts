b0b636e4a5d4771d188816b3b0f18984cc5b55f3
Jhonatan David Garcia <jhonatan.1701811787@ucaldas.edu.co>
2021-07-28 17:57:51 -0500

    formulario de cliente

--- src/app/modulos/usuario/crear-usuario/crear-usuario.component.ts
+++ src/app/modulos/usuario/crear-usuario/crear-usuario.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/servicios/usuario.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UsuarioModelo } from 'src/app/modelos/usuario.modelo';
 
@@ -10,7 +10,7 @@ import { UsuarioModelo } from 'src/app/modelos/usuario.modelo';
   styleUrls: ['./crear-usuario.component.css']
 })
 export class CrearUsuarioComponent implements OnInit {
-
+  
   fgValidador: FormGroup = new FormGroup({});
 
   constructor(
@@ -21,13 +21,12 @@ export class CrearUsuarioComponent implements OnInit {
 
   ConstruirFormulario() {
     this.fgValidador = this.fb.group({
-      Documento: ['', [Validators.required]],
       Nombre: ['', [Validators.required]],
       Apellido: ['', [Validators.required]],
+      Documento: ['', [Validators.required]],
+      Celular: ['', [Validators.required]],
       Correo: ['', [Validators.required]],
-      Celular:['', [Validators.required]],
       Ciudad:['', [Validators.required]],
-      Contrasena:['', [Validators.required]],
       Rol:['', [Validators.required]]
     });
   }
@@ -42,24 +41,20 @@ export class CrearUsuarioComponent implements OnInit {
 
   GuardarRegistro(){
     let nom = this.ObtenerFgValidator.nombre.value;
-    let id = this.ObtenerFgValidator.id.value;
     let ape = this.ObtenerFgValidator.ape.value;
     let correo = this.ObtenerFgValidator.correo.value;
     let celular = this.ObtenerFgValidator.celular.value;
     let doc = this.ObtenerFgValidator.doc.value;
-    let contras = this.ObtenerFgValidator.contras.value;
     let rol = this.ObtenerFgValidator.rol.value;
     let ciudad = this.ObtenerFgValidator.ciudad.value;
 
     let modelo: UsuarioModelo = new UsuarioModelo();
 
-    modelo.Id_usuario = id;
     modelo.Nombre = nom;
     modelo.Apellido = ape;
     modelo.Correo = correo;
     modelo.Celular = celular;
     modelo.Ciudad = ciudad;
-    modelo.Contrasena = contras;
     modelo.Rol = rol;
     modelo.Documento = doc;
 
