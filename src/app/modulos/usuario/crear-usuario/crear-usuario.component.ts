486b86add53322ccd437109e72d4dc9676e07414
Jhonatan David Garcia <jhonatan.1701811787@ucaldas.edu.co>
2021-07-28 20:43:30 -0500

    agregar ususario

--- src/app/modulos/usuario/crear-usuario/crear-usuario.component.ts
+++ src/app/modulos/usuario/crear-usuario/crear-usuario.component.ts
@@ -2,7 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/servicios/usuario.service';
 import { FormBuilder, FormGroup, Validators,FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CiudadService } from 'src/app/servicios/ciudad.service';
 import { UsuarioModelo } from 'src/app/modelos/usuario.modelo';
+import { CiudadModelo } from 'src/app/modelos/ciudad.modelo';
+
+declare var IniciarSelect: any;
 
 @Component({
   selector: 'app-crear-usuario',
@@ -12,27 +16,40 @@ import { UsuarioModelo } from 'src/app/modelos/usuario.modelo';
 export class CrearUsuarioComponent implements OnInit {
   
   fgValidador: FormGroup = new FormGroup({});
+  listaCiudades: CiudadModelo[] = [];
 
   constructor(
     private fb: FormBuilder,
     private servicio: UsuarioService,
+    private CiudadServicio: CiudadService,
     private router: Router
   ) { }
 
   ConstruirFormulario() {
     this.fgValidador = this.fb.group({
-      Nombre: ['', [Validators.required]],
-      Apellido: ['', [Validators.required]],
-      Documento: ['', [Validators.required]],
-      Celular: ['', [Validators.required]],
-      Correo: ['', [Validators.required]],
-      Ciudad:['', [Validators.required]],
-      Rol:['', [Validators.required]]
+      nombre: ['', [Validators.required]],
+      ape: ['', [Validators.required]],
+      doc: ['', [Validators.required]],
+      celular: ['', [Validators.required]],
+      correo: ['', [Validators.required,Validators.email]],
+      ciudad:['', [Validators.required]],
+      rol:['', [Validators.required]]
     });
   }
 
   ngOnInit(): void {
     this.ConstruirFormulario();
+    this.CiudadServicio.ListarRegistros().subscribe(
+      (datos) =>{
+        this.listaCiudades = datos;
+        setTimeout(() => {
+          IniciarSelect();
+        }, 500);
+      },
+      (erro) =>{
+        alert("error cargando las ciudades")
+      }
+    );
   }
 
   get ObtenerFgValidator(){
@@ -43,11 +60,12 @@ export class CrearUsuarioComponent implements OnInit {
     let nom = this.ObtenerFgValidator.nombre.value;
     let ape = this.ObtenerFgValidator.ape.value;
     let correo = this.ObtenerFgValidator.correo.value;
-    let celular = this.ObtenerFgValidator.celular.value;
-    let doc = this.ObtenerFgValidator.doc.value;
-    let rol = this.ObtenerFgValidator.rol.value;
+    let celular = parseInt(this.ObtenerFgValidator.celular.value);
+    let doc = parseInt(this.ObtenerFgValidator.doc.value);
     let ciudad = this.ObtenerFgValidator.ciudad.value;
-
+    let rol = this.ObtenerFgValidator.rol.value;
+    
+    console.log(nom+ape+correo+celular+doc+rol+ciudad)
     let modelo: UsuarioModelo = new UsuarioModelo();
 
     modelo.Nombre = nom;
@@ -57,11 +75,11 @@ export class CrearUsuarioComponent implements OnInit {
     modelo.Ciudad = ciudad;
     modelo.Rol = rol;
     modelo.Documento = doc;
-
+    console.log(modelo)
     this.servicio.AlmacenarRegistro(modelo).subscribe(
       (datos) => {
         alert("Registro almacenado correctamente");
-        this.router.navigate(["/parametrizacion/listar-usuario"])
+        this.router.navigate(["/usuario/listar-usuario"])
       },
       (err) => {
         alert("Error almacenando el registro");
